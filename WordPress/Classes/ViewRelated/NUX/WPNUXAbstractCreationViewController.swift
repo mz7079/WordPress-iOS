bd7c1c68958eb7841c04a7705541e6fa28eb8600
Will Kwon <will@willkwon.com>
2015-11-19 12:18:49 -0800

    Adding keyboardNotification handling

--- WordPress/Classes/ViewRelated/NUX/WPNUXAbstractCreationViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/WPNUXAbstractCreationViewController.swift
@@ -2,11 +2,25 @@ import UIKit
 
 class WPNUXAbstractCreationViewController: UIViewController
 {
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.registerForKeyboardNotifications()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    func registerForKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardDidShow", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide", name: UIKeyboardDidHideNotification, object: nil)
+    }
+    
+    func keyboardDidShow(notification: NSNotification) {
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        
     }
 }
