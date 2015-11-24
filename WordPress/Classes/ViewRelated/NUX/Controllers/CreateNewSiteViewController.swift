4162166e8d27ead0abf46f0385fd8da2928716cb
Will Kwon <will@willkwon.com>
2015-11-24 08:53:38 -0800

    Adding text field (empty for right now, but it works)

--- WordPress/Classes/ViewRelated/NUX/Controllers/CreateNewSiteViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/Controllers/CreateNewSiteViewController.swift
@@ -5,6 +5,15 @@ class CreateNewSiteViewController: WPNUXAbstractCreationViewController
     override func viewDidLoad() {
         super.viewDidLoad()
         icon.hidden = true
-        titleLabel.attributedText = NSAttributedString(string: NSLocalizedString("Create a new WordPress.com site", comment: "Create a new WordPress.com site title"), attributes: WPNUXUtility.titleAttributesWithColor(UIColor.whiteColor()) as! [String: AnyObject])
+        let titleAttributes: [String : AnyObject] = WPNUXUtility.titleAttributesWithColor(UIColor.whiteColor()) as! [String : AnyObject]
+        titleLabel.attributedText = NSAttributedString(string: NSLocalizedString("Create a new WordPress.com site", comment: "Create a new WordPress.com site title"), attributes: titleAttributes)
+        addTextFields()
+    }
+    
+    func addTextFields() {
+        let emailField = WPWalkthroughTextField(leftViewImage: UIImage(named: "icon-email-field"))
+        emailField.backgroundColor = UIColor.whiteColor()
+        textFields.addArrangedSubview(emailField)
+
     }
 }
