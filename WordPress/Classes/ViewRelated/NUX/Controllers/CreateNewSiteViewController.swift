41d9acf5634f1a3bf234eec8c89142b15746b3c8
Will Kwon <will@willkwon.com>
2015-12-03 16:26:54 -0500

    adding site title and site address fields

--- WordPress/Classes/ViewRelated/NUX/Controllers/CreateNewSiteViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/Controllers/CreateNewSiteViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class CreateNewSiteViewController: WPNUXAbstractCreationViewController
 {
+    let sizePadding: CGFloat = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         icon.hidden = true
@@ -26,7 +28,32 @@ class CreateNewSiteViewController: WPNUXAbstractCreationViewController
         siteTitleField.accessibilityLabel = NSLocalizedString("Title", comment: "Title field")
         siteTitleField.returnKeyType = .Next
         
+        let siteAddressField = WPWalkthroughTextField(leftViewImage: UIImage(named: "icon-url-field"))
+        siteTitleField.backgroundColor = UIColor.whiteColor()
+        siteTitleField.placeholder = NSLocalizedString("Site Address", comment: "Site Address")
+        siteTitleField.font = WPNUXUtility.textFieldFont()
+        siteTitleField.adjustsFontSizeToFitWidth = true
+        siteTitleField.delegate = self
+        siteTitleField.autocorrectionType = .No
+        siteTitleField.autocapitalizationType = .None
+        siteTitleField.accessibilityLabel = NSLocalizedString("Title", comment: "Title field")
+        siteTitleField.returnKeyType = .Done
+        siteTitleField.showTopLineSeparator = true
+        
+        let siteAddressWPComLabel = UILabel()
+        siteAddressWPComLabel.text = ".wordpress.com"
+        siteAddressWPComLabel.textAlignment = .Center
+        siteAddressWPComLabel.font = WPNUXUtility.descriptionTextFont()
+        siteAddressWPComLabel.textColor = WPStyleGuide.allTAllShadeGrey()
+        siteAddressWPComLabel.sizeToFit()
+        
+        var siteAddressTextInsets = siteAddressField.textInsets
+        siteAddressTextInsets.right += siteAddressWPComLabel.frame.size.width + sizePadding
+        siteAddressField.textInsets = siteAddressTextInsets
+        siteAddressField.addSubview(siteAddressWPComLabel)
+
         textFieldsArray.append(siteTitleField)
+        textFieldsArray.append(siteAddressField)
         
         return textFieldsArray
     }
